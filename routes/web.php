945ab134a9734efb08dd6d93f97e75c94835ae00
shoya <shoyamoco021729@gmail.com>
2023-04-14 11:15:34 +0000

    課題5.6の提出

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
     Route::get('news/edit', 'edit');
 });
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
