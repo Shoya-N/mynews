58c957ba6648f382e22f4005c19547d9ae8308aa
shoya <shoyamoco021729@gmail.com>
2023-04-15 11:18:02 +0000

    laravel08課題

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -10,6 +10,41 @@
        <div class="row">
            <div class="col-md-8 mx-auto">
                <h2>NBAの試合結果</h2>
+               <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">日付</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="date" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">チーム</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="team">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">試合内容</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="gender" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">画像</label>
+                        <div class="col-md-10">
+                            <input type="file" class="form-control-file" name="image">
+                        </div>
+                    </div>
+                    @csrf
+                    <input type="submit" class="btnbtn-primary" value="更新">
+                </form>
            </div>
        </div>
    </div>
